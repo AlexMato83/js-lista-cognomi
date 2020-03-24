f43fe36b138fc2f9e95f2cf8c27433582d8e2109
AlexMato <matontialessandro0@gmail.com>
2020-03-24 14:14:58 +0100

    inizio ex

---
+++ main.js
@@ -0,0 +1,13 @@
+/*Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+Scrivi anche la posizione della lista in cui il nuovo utente si trova */
+
+//chiedere il cognome all'utente
+var newSurname = prompt("Prego inserirca il suo cognome")
+//creare array di cognomi
+var listSurname = ["Montanari", "Randazzo", "Fraternali", "Fiaschi"]
+
+//inserire cognome nell'array
+
+//stampare l'array in ordine alfabetico
+
+//scrivere posizione della lista del cognome inserito
