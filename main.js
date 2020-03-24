9e2bf9428881aa22362a91cff07379aaaf6b6613
AlexMato <matontialessandro0@gmail.com>
2020-03-24 15:28:13 +0100

    ordine alfabetico lista

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ Scrivi anche la posizione della lista in cui il nuovo utente si trova */
 var newSurname = prompt("Prego inserirca il suo cognome")
 //creare array di cognomi
 var listSurname = ["Montanari", "Randazzo", "Fraternali", "Fiaschi"]
+console.log(listSurname);
 
 //inserire cognome nell'array
+var listSurname2 = listSurname.push(newSurname);
+console.log(listSurname);
 
 //stampare l'array in ordine alfabetico
-
+var ordinamento = listSurname2.sort();
+console.log(ordinamento);
 //scrivere posizione della lista del cognome inserito
